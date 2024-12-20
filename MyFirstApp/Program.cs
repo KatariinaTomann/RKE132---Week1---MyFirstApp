70a54cf2488f6a1bfee76ec2f28e6eef668f6b37
KatariinaTomann <katariina.tomann@tktk.ee>
2024-12-20 11:30:21 +0200

    Add project files.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See on kommentaar mu koodile
+
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+
+Console.WriteLine("Enter your name:");//Output
+//string - sõne
+string userName; //Input
+
+userName = Console.ReadLine(); //insput
+
+Console.WriteLine("Hello" + ", " + userName + "!");//output
+                                                   //string interpolation
